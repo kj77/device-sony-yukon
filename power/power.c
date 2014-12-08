89bff7ac1ac9a13db53a12aa39dcc6f4c6558014
Steve Kondik <steve@cyngn.com>
2014-12-08 01:37:30 -0800

    power: Add support for POWER_HINT_LOW_POWER
     * Just call set_power_profile
     * Also clean up all the warnings
    
    Change-Id: Ia42badac37815e29411ad135716a0f791a00c510

--- power/power.c
+++ power/power.c
@@ -62,7 +62,7 @@ static struct hw_module_methods_t power_module_methods = {
     .open = NULL,
 };
 
-static void power_init(struct power_module *module)
+static void power_init(__attribute__((unused))struct power_module *module)
 {
     ALOGI("QCOM power HAL initing.");
 
@@ -189,13 +189,15 @@ static void process_video_encode_hint(void *metadata)
     }
 }
 
-int __attribute__ ((weak)) power_hint_override(struct power_module *module, power_hint_t hint,
-        void *data)
+int __attribute__ ((weak)) power_hint_override(
+        __attribute__((unused)) struct power_module *module,
+        __attribute__((unused)) power_hint_t hint,
+        __attribute__((unused)) void *data)
 {
     return HINT_NONE;
 }
 
-static void power_hint(struct power_module *module, power_hint_t hint,
+static void power_hint(__attribute__((unused)) struct power_module *module, power_hint_t hint,
         void *data)
 {
     pthread_mutex_lock(&hint_mutex);
@@ -211,6 +213,7 @@ static void power_hint(struct power_module *module, power_hint_t hint,
         case POWER_HINT_INTERACTION:
         case POWER_HINT_CPU_BOOST:
         case POWER_HINT_SET_PROFILE:
+        case POWER_HINT_LOW_POWER:
         break;
         case POWER_HINT_VIDEO_ENCODE:
             process_video_encode_hint(data);
@@ -224,7 +227,9 @@ out:
     pthread_mutex_unlock(&hint_mutex);
 }
 
-int __attribute__ ((weak)) set_interactive_override(struct power_module *module, int on)
+int __attribute__ ((weak)) set_interactive_override(
+        __attribute__((unused)) struct power_module *module,
+        __attribute__((unused)) int on)
 {
     return HINT_NONE;
 }
@@ -445,7 +450,7 @@ struct power_module HAL_MODULE_INFO_SYM = {
         .hal_api_version = HARDWARE_HAL_API_VERSION,
         .id = POWER_HARDWARE_MODULE_ID,
         .name = "QCOM Power HAL",
-        .author = "Qualcomm",
+        .author = "Qualcomm/CyanogenMod",
         .methods = &power_module_methods,
     },
 
